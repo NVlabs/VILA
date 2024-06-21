7d817fab3e04cb120c290eacf1b74491de688898
poorfrombabylon <al06maz2002@gmail.com>
2024-06-21 23:54:38 +0300

    added functionality to process a bunch of videos at a time

--- llava/eval/run_vila.py
+++ llava/eval/run_vila.py
@@ -41,25 +41,114 @@ def load_images(image_files):
         out.append(image)
     return out
 
-def eval_model(args):
+
+# def eval_model(args):
+#     # Model
+#     disable_torch_init()
+#     if args.video_file is None:
+#         image_files = image_parser(args)
+#         images = load_images(image_files)
+#     else:
+#         if args.video_file.startswith("http") or args.video_file.startswith("https"):
+#             print("downloading video from url", args.video_file)
+#             response = requests.get(args.video_file)
+#             video_file = BytesIO(response.content)
+#         else:
+#             assert osp.exists(args.video_file), "video file not found"
+#             video_file = args.video_file
+#         from llava.mm_utils import opencv_extract_frames
+#         images = opencv_extract_frames(video_file, args.num_video_frames)
+#
+#     model_name = get_model_name_from_path(args.model_path)
+#
+#     tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, model_name, args.model_base)
+#
+#     qs = args.query
+#     image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+#     if IMAGE_PLACEHOLDER in qs:
+#         if model.config.mm_use_im_start_end:
+#             qs = re.sub(IMAGE_PLACEHOLDER, image_token_se, qs)
+#         else:
+#             qs = re.sub(IMAGE_PLACEHOLDER, DEFAULT_IMAGE_TOKEN, qs)
+#     else:
+#         if DEFAULT_IMAGE_TOKEN not in qs:
+#             print("no <image> tag found in input. Automatically append one at the beginning of text.")
+#             # do not repeatively append the prompt.
+#             if model.config.mm_use_im_start_end:
+#                 qs = (image_token_se + "\n") * len(images) + qs
+#             else:
+#                 qs = (DEFAULT_IMAGE_TOKEN + "\n") * len(images) + qs
+#     print("input: ", qs)
+#
+#     if "llama-2" in model_name.lower():
+#         conv_mode = "llava_llama_2"
+#     elif "v1" in model_name.lower():
+#         conv_mode = "llava_v1"
+#     elif "mpt" in model_name.lower():
+#         conv_mode = "mpt"
+#     else:
+#         conv_mode = "llava_v0"
+#
+#     if args.conv_mode is not None and conv_mode != args.conv_mode:
+#         print(
+#             "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+#                 conv_mode, args.conv_mode, args.conv_mode
+#             )
+#         )
+#     else:
+#         args.conv_mode = conv_mode
+#
+#     conv = conv_templates[args.conv_mode].copy()
+#     conv.append_message(conv.roles[0], qs)
+#     conv.append_message(conv.roles[1], None)
+#     prompt = conv.get_prompt()
+#
+#     images_tensor = process_images(images, image_processor, model.config).to(model.device, dtype=torch.float16)
+#     input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).cuda()
+#
+#     stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+#     keywords = [stop_str]
+#     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+#
+#     print(images_tensor.shape)
+#     with torch.inference_mode():
+#         output_ids = model.generate(
+#             input_ids,
+#             images=[
+#                 images_tensor,
+#             ],
+#             do_sample=True if args.temperature > 0 else False,
+#             temperature=args.temperature,
+#             top_p=args.top_p,
+#             num_beams=args.num_beams,
+#             max_new_tokens=args.max_new_tokens,
+#             use_cache=True,
+#             stopping_criteria=[stopping_criteria],
+#         )
+#
+#     outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
+#     outputs = outputs.strip()
+#     if outputs.endswith(stop_str):
+#         outputs = outputs[: -len(stop_str)]
+#     outputs = outputs.strip()
+#     print(outputs)
+
+
+def load_vila_model(args):
     # Model
     disable_torch_init()
-    if args.video_file is None:
-        image_files = image_parser(args)
-        images = load_images(image_files)
-    else:
-        if args.video_file.startswith("http") or args.video_file.startswith("https"):
-            print("downloading video from url", args.video_file)
-            response = requests.get(args.video_file)
-            video_file = BytesIO(response.content)
-        else:
-            assert osp.exists(args.video_file), "video file not found"
-            video_file = args.video_file
-        from llava.mm_utils import opencv_extract_frames
-        images = opencv_extract_frames(video_file, args.num_video_frames)
-        
     model_name = get_model_name_from_path(args.model_path)
+
     tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, model_name, args.model_base)
+
+    return tokenizer, model, image_processor, context_len, model_name
+
+
+def predict_captions(tokenizer, model, image_processor, context_len, model_name, video_file):
+    assert osp.exists(video_file), "video file not found"
+
+    from llava.mm_utils import opencv_extract_frames
+    images = opencv_extract_frames(video_file, args.num_video_frames)
 
     qs = args.query
     image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
@@ -101,9 +190,6 @@ def eval_model(args):
     conv.append_message(conv.roles[1], None)
     prompt = conv.get_prompt()
 
-    
-        
-        
     images_tensor = process_images(images, image_processor, model.config).to(model.device, dtype=torch.float16)
     input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).cuda()
 
@@ -140,7 +226,7 @@ if __name__ == "__main__":
     parser.add_argument("--model-path", type=str, default="Efficient-Large-Model/VILA-2.7b")
     parser.add_argument("--model-base", type=str, default=None)
     parser.add_argument("--image-file", type=str, default=None)
-    parser.add_argument("--video-file", type=str, default=None)
+    parser.add_argument("--parent-folder", type=str, default=None)
     parser.add_argument("--num-video-frames", type=int, default=6)
     parser.add_argument("--query", type=str, required=True)
     parser.add_argument("--conv-mode", type=str, default=None)
@@ -151,4 +237,15 @@ if __name__ == "__main__":
     parser.add_argument("--max_new_tokens", type=int, default=512)
     args = parser.parse_args()
 
-    eval_model(args)
+    # eval_model(args)
+
+    tokenizer, model, image_processor, context_len, model_name = load_vila_model(args)
+
+    for root, _, files in os.walk(args.parent_folder):
+        for file in files:
+            if file.endswith(".mp4"):  # Check if the file is an mp4 video
+                current_folder = root
+                file_name = file
+                video_path = f"{current_folder}/{file_name}"
+
+                predict_captions(tokenizer, model, image_processor, context_len, model_name, video_path)
