0da1836687a468ee06765330820e7c1325aa08e2
Tonyhao96 <673665244@qq.com>
2024-08-21 13:16:20 +0000

    update header

--- llava/train/sequence_parallel/all_to_all.py
+++ llava/train/sequence_parallel/all_to_all.py
@@ -3,6 +3,9 @@
 
 # DeepSpeed Team
 
+# Adopted from https://github.com/feifeibear/long-context-attention
+# Implementation refers to USP Paper: https://arxiv.org/abs/2405.07719
+
 from typing import Any, Tuple
 
 import torch
