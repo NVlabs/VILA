7c499298449906c04d95f074e2a81988c7d15998
yukang <yukangchen@cse.cuhk.edu.hk>
2024-12-23 18:35:15 +0800

    Update __init__.py

--- llava/eval/__init__.py
+++ llava/eval/__init__.py
@@ -5,5 +5,5 @@ from llava.utils import io
 __all__ = ["EVAL_ROOT", "TASKS"]
 
 
-EVAL_ROOT = "scripts/v1_5/eval"
+EVAL_ROOT = "scripts/eval"
 TASKS = io.load(os.path.join(os.path.dirname(__file__), "registry.yaml"))
